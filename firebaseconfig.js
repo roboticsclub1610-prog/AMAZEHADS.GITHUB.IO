c661c73420addf59ed6a65d462f2cf164cdb5778
roboticsclub1610-prog <roboticsclub1610@gmail.com>
2025-11-27 20:45:58 +0530

    Create firebaseconfig.js

---
+++ firebaseconfig.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDroA-DvbgVa_qTwzjetI10eDwSlJQ7mhw",
+  authDomain: "exhibition-3688c.firebaseapp.com",
+  projectId: "exhibition-3688c",
+  storageBucket: "exhibition-3688c.firebasestorage.app",
+  messagingSenderId: "331251889266",
+  appId: "1:331251889266:web:30b3e2793d40774d66beeb",
+  measurementId: "G-JV39PPYFMF"
+};
